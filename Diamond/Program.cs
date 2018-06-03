d893a7fe2a6da01f8a066905dfd29f3da55337c3
Warut <s5602041620060@email.kmutnb.ac.th>
2018-06-03 22:50:15 +0700

    First diamond pattern

---
+++ Diamond/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, r;
+
+            Console.Write("\n\n");
+            Console.Write("Display the pattern like diamond:\n");
+            Console.Write("-----------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input number of rows (half of the diamond) :");
+            r = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= r; i++)
+            {
+                for (j = 1; j <= r - i; j++)
+                    Console.Write(" ");
+                for (j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+
+            for (i = r - 1; i >= 1; i--)
+            {
+                for (j = 1; j <= r - i; j++)
+                    Console.Write(" ");
+                for (j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
